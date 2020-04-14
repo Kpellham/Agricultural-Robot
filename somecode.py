21c38d1941228768468c9611acd9e5f8cd6a5637
Kpellham <kpellham@eagles.ewu.edu>
2020-04-13 19:43:48 -0700

    setting up some actual code for the ev3

--- somecode.py
+++ somecode.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env pybricks-micropython
-from ev3dev.auto import *
+from ev3dev2.auto import *
 
-m = Motor(OUTPUT_A)
-m.run_timed(time_sp=3000, speed_sp=500)
+m1 = Motor(OUTPUT_A)
+m2 = Motor(OUTPUT_B)
+m1.run_timed(time_sp=3000, speed_sp=500)
+m2.run_timed(time_sp=3000, speed_sp=500)
