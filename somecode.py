aeb9e7942c2a0c6c3cbcff8577a693b1ec2f7846
Kpellham <kpellham@eagles.ewu.edu>
2020-04-20 20:27:32 -0700

    code submitted for 499

--- somecode.py
+++ somecode.py
@@ -1,7 +1,8 @@
-#!/usr/bin/micropython
-from ev3dev2.auto import *
+#!/usr/bin/python3
+#the above command is a linux directive that tells Linux to run the following
+#code as python script
+from ev3dev2.auto import *#This imports every library from the ev3 library
 
-m1 = Motor(OUTPUT_A)
-m2 = Motor(OUTPUT_B)
-m1.run_timed(time_sp=3000, speed_sp=500)
-m2.run_timed(time_sp=3000, speed_sp=500)
+m1 = Motor(OUTPUT_A)#this command tells the the EV3 brick that a motor will be
+#connected to output a
+m1.run_timed(time_sp=7000, speed_sp=750)#this command runs the arm for 7 seconds
