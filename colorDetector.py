164b09a39c4c2ebd195b7ce9f50f418fe31904c3
Scott <scott.mckinley3@gmail.com>
2020-05-09 20:59:43 -0700

    Message

---
+++ colorDetector.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import time
+from ev3dev2.auto import *
+import numpy as np
+
+colorSens = ColorSensor(INPUT_2)
+colorSens.calibrate_white()
+
+samp = np.zeros((25, 3))
+
+for i in range(0, 24):
+    samp([i,0]) += colorSens.rgb
+
+
