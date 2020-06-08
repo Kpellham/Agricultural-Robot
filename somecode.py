3efdafd340efa0041acbd6c2f554dcfe0fc6ce3d
Kpellham <kpellham@eagles.ewu.edu>
2020-06-07 18:40:46 -0700

    Class to allow for abstracted tank movement

--- somecode.py
+++ somecode.py
@@ -2,7 +2,30 @@
 #the above command is a linux directive that tells Linux to run the following
 #code as python script
 from ev3dev2.auto import *#This imports every library from the ev3 library
+import tankMovement as tk
+from kalman import OneVarKalmanFilter
+def findPeeks(x):
+    minix = 0
+    min = 100
+    for i in range(0, len(x)-1):
+        if x[i]
+def getNumApp(m1,m2,s1):
+    A = 1
+    B = 0
+    C = 1
+    Q = 0.005
+    R = 1
+    x = 50
+    P = 1
 
-m1 = Motor(OUTPUT_A)#this command tells the the EV3 brick that a motor will be
-#connected to output a
-m1.run_timed(time_sp=7000, speed_sp=750)#this command runs the arm for 7 seconds
+    kalman_filter = OneVarKalmanFilter(A, B, C, x, P, Q, R)
+
+    ld = []
+    ld2=0
+    tk.turnZdeg(m1,m2,300, 45)
+    for i in range(0, 89):
+       tk.turnZdeg(m1,m2,150,1)
+       for i2 in range(0,99):
+          ld2 = kalman_filter.step(s1.proximity)
+        ld.append(ld2)
+
